8227c9d2fbc3ea67475bb97fa5113871b6d81b51
Chumki khatun <chumkikhatun370@gamil.com>
2023-09-11 21:48:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <h1 className='text-4xl text-red-500'>Knowledge-cafe </h1>
+      
+      
+    </>
+  )
+}
+
+export default App
